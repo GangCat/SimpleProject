f348451dd80b541a3792e312e99c545c0828a6a4
GangCat <windy9300@gmail.com>
2025-05-14 19:45:00 +0900

    적 생성시 성으로 이동, 성과 충돌시 일단 성 비활성화

--- Assets/Scripts/TempEnemy.cs
+++ Assets/Scripts/TempEnemy.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class TempEnemy : MonoBehaviour
@@ -10,10 +11,16 @@ public class TempEnemy : MonoBehaviour
 
     private ObjectPool pool = null;
 
+    private bool isAlive = false;
+
+    private Vector2 castlePos = Vector2.zero;
+
     public void Init(ObjectPool _pool, Vector2 _pos)
     {
         pool = _pool;
         transform.position = _pos;
+        isAlive = true;
+        StartCoroutine(nameof(MoveToCastleCoroutine));
     }
 
     public void Damaged(float dmg)
@@ -21,7 +28,25 @@ public class TempEnemy : MonoBehaviour
         curHp -= dmg;
         if(curHp <= 0)
         {
+            isAlive = false;
             pool.DeactivatePoolItem(gameObject);
         }
+    }
+
+    private IEnumerator MoveToCastleCoroutine()
+    {
+        // 방향은 항상 0,0 방향이기 때문에 자기 포지션의 역임
+        Vector2 dir = -transform.position;
+        while(isAlive)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Vector2.zero, 1f * Time.deltaTime);
+
+            yield return null;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.SetActive(false);
     }
 }
